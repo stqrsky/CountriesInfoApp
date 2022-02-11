39240028bece03c3913b7bef8bd9d15fcdae0711
Starsky <starsky.m@live.com>
2022-02-11 12:34:21 +0100

    custom table view cells

--- CountriesInfoApp/ViewController.swift
+++ CountriesInfoApp/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["Deutschland", "Philippinen", "Frankreich", "Dänemark"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight  = 75
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CountryTableViewCell
+        
+        cell.countryNameLabel.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
